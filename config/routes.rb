1d4411ba72641a5bdfa061b255c7fb67164949bf
Shih_Kuan <cliff1304@gmail.com>
2018-05-22 15:22:22 +0800

    建立使用者/商品/後台路由。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Phase 1：商品展示
+  resources :users, only: [:edit, :update, :show]
+
+  resources :products
+
+  root "products#index"
+
+  namespace :quisveniam do
+    resources :products
+    root "products#index"
+  end
+
+  # Phase 2：商品與購物車 carts
+  # Phase 3：成立訂單 orders
+  # Phase 4：線上支付 payments
+
 end
