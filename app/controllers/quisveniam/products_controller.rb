470b98a7668fd946b444b38462ce9787741601cf
Shih_Kuan <cliff1304@gmail.com>
2018-05-24 16:24:25 +0800

    完成後台商品頁面 Create/Update/Delete。

--- app/controllers/quisveniam/products_controller.rb
+++ app/controllers/quisveniam/products_controller.rb
@@ -1,5 +1,56 @@
 class Quisveniam::ProductsController < Quisveniam::BaseController
+  before_action :set_product, only: [:edit, :update, :destroy]
+
   def index
     @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash[:notice] = "Product was created successfully."
+      redirect_to quisveniam_products_path
+    else
+      flash[:alert] = "Product was failed to create."
+      render :new
+    end
+  end
+
+  def edit
+    # set_product
+  end
+
+  def update
+    if @product.update(product_params)
+      flash[:notice] = "Update product successfully."
+      redirect_to quisveniam_products_path
+    else
+      flash[:alert] = "Update product successfully."
+      render :edit
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      flash[:notice] = "Product was deleted successfully."
+      redirect_to quisveniam_products_path
+    else
+      flash[:alert] = "Product was failed to delete!"
+      redirect_to quisveniam_products_path
+    end
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :image)
   end
 end
