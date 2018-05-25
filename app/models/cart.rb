fbff9105c0e5e17d501ad21e99bcccb8ca481825
Shih_Kuan <cliff1304@gmail.com>
2018-05-25 20:14:22 +0800

    完成計算購物車總和方法。

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
-  
+
   def add_cart_item(product)
     existing_item = self.cart_items.find_by(product: product)
     if existing_item
@@ -13,5 +13,14 @@ class Cart < ApplicationRecord
     end
 
     return self.cart_items
+  end
+
+  def subtotal
+    sum = 0
+    cart_items.each do |cart_item|
+      sum += cart_item.product.price * cart_item.quantity
+    end
+
+    return sum
   end
 end
