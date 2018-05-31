2243309f5e1e2bd2eee32c54c3e6ea209af76ca9
Shih_Kuan <cliff1304@gmail.com>
2018-05-31 12:14:35 +0800

    後台更新訂單爲已付款或是已出貨狀態時，會寄出通知信至使用者的註冊信箱。

--- app/controllers/quisveniam/orders_controller.rb
+++ app/controllers/quisveniam/orders_controller.rb
@@ -11,6 +11,15 @@ class Quisveniam::OrdersController < ApplicationController
     @order = Order.find(params[:id])
 
     @order.update(order_params)
+
+    if order_params[:payment_status] == "paid"
+      # 寄訂單已付款通知信至使用者的註冊信箱
+      UserMailer.notify_order_paid(@order).deliver_now!
+    elsif order_params[:shipping_status] == "shipped"
+      # 寄訂單已出貨通知信至使用者的註冊信箱
+      UserMailer.notify_order_shipped(@order).deliver_now!
+    end
+
     redirect_to edit_quisveniam_order_path(@order.id), notice: "Order updated."
   end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,4 +9,22 @@ class UserMailer < ApplicationMailer
     mail to: order.user.email,
     subject: "購物車練習 | sn：#{@order.sn} 訂單成立"
   end
+
+  def notify_order_paid(order)
+    @order = order
+    @content = "Your order##{@order.sn} is paid. We will ship soon."
+    @username = order.user.name
+
+    mail to: order.user.email,
+    subject: "購物車練習 | sn：#{@order.sn} 訂單已付款"
+  end
+
+  def notify_order_shipped(order)
+    @order = order
+    @content = "Your order##{@order.sn} is shipped."
+    @username = order.user.name
+
+    mail to: order.user.email,
+    subject: "購物車練習 | sn：#{@order.sn} 訂單已出貨"
+  end
 end
