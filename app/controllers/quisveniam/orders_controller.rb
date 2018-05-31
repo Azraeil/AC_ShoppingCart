e117a058a601c736c4cafd7c4a9485647d862b97
Shih_Kuan <cliff1304@gmail.com>
2018-05-31 11:47:09 +0800

    完成後台訂單瀏覽/編輯/更新。

--- app/controllers/quisveniam/orders_controller.rb
+++ app/controllers/quisveniam/orders_controller.rb
@@ -1,2 +1,21 @@
 class Quisveniam::OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+
+    @order.update(order_params)
+    redirect_to edit_quisveniam_order_path(@order.id), notice: "Order updated."
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_status, :shipping_status)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,14 @@ class Order < ApplicationRecord
   has_many :products, through: :order_items
   belongs_to :user
 
+  # set constant to generate select options
+  PAYMENT_STATUS = [
+    ["Not Paid", :not_paid],
+    ["Paid", :paid]
+  ]
+
+  SHIPPING_STATUS = [
+    ["Not Shipped", :not_shipped],
+    ["Shipped", :shipped]
+  ]
 end
